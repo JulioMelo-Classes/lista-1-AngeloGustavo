4def23fdda153154ef550c5e1a0fd2d825cb5fc5
Angelo <angelogustavo157@gmail.com>
2021-06-12 00:15:34 +0000

    Q8

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -15,7 +15,17 @@ using std::max;
 #include "function.h"
 
 //=== Funções
-
+int pt_in_rect ( const Ponto *R1, const Ponto *R2, const Ponto *P ){
+  if(P->x > R1->x 
+  && P->x < R2->x 
+  && P->y > R1->y
+  && P->y < R2->y) 
+    return INSIDE;
+  if(((P->x >= R1->x && P->x <= R2->x)&&((P->y == R1->y)||(P->y == R2->y)))
+  ||((P->y >= R1->y && P->y <= R2->y)&&((P->x == R1->x)||(P->x == R2->x))))
+    return BORDER; 
+  return OUTSIDE;
+}
 // Coloque aqui qualquer função auxiliar que desejar.
 
 
