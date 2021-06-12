d7f43e3e041d1de8775fc188063c8ac4fbf83a14
Angelo <angelogustavo157@gmail.com>
2021-06-12 00:07:58 +0000

    Q8

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -19,9 +19,40 @@ using std::max;
 // Coloque aqui qualquer função auxiliar que desejar.
 
 
-int main(void)
+int main( void )
 {
-    // TODO: Adicione aqui seu código.
+  int res,aux;
+  Ponto R1,R2,P;
+  int x1,y1,x2,y2,px,py;
+  while( (cin >> std::ws >> R1.x) 
+  && (cin >> std::ws >> R1.y)
+  && (cin >> std::ws >> R2.x)
+  && (cin >> std::ws >> R2.y)
+  && (cin >> std::ws >> P.x)
+  && (cin >> std::ws >> P.y)) {
+    //cout << x1 << y1 << x2 << y2 << px << py <<endl;
+    if(R1.x>R2.x){
+      aux = R1.x;
+      R1.x = R2.x;
+      R2.x = aux;
+    }if(R1.y>R2.y){
+      aux = R1.y;
+      R1.y = R2.y;
+      R2.y = aux;
+    }
 
-    return 0;
+    if(R1.x == R2.x && R1.y == R2.y)
+      cout << "invalid\n";
+    else{
+      res = pt_in_rect(&R1,&R2,&P);
+      if(res==0)
+        cout << "inside\n";
+      else if(res==1)
+        cout << "border \n";
+      else
+        cout << "outside\n";
+    }
+  }
+
+  return 0;
 }
