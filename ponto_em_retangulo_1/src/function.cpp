8025e20cc9ac424712ae8b1dbeb42a08b1cdf402
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-15 17:49:17 -0300

    Q7

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -4,14 +4,14 @@
  * Verifica se um ponto está dentro de um retângulo.
  */
 
-int pt_in_rect ( const Ponto *R1, const Ponto *R2, const Ponto *P ){
-  if(P->x > R1->x 
-  && P->x < R2->x 
-  && P->y > R1->y
-  && P->y < R2->y) 
+location_t pt_in_rect( const Ponto& R1, const Ponto& R2, const Ponto& P){
+  if(P.x > R1.x 
+  && P.x < R2.x 
+  && P.y > R1.y
+  && P.y < R2.y) 
     return INSIDE;
-  if(((P->x >= R1->x && P->x <= R2->x)&&((P->y == R1->y)||(P->y == R2->y)))
-  ||((P->y >= R1->y && P->y <= R2->y)&&((P->x == R1->x)||(P->x == R2->x))))
+  if(((P.x >= R1.x && P.x <= R2.x)&&((P.y == R1.y)||(P.y == R2.y)))
+  ||((P.y >= R1.y && P.y <= R2.y)&&((P.x == R1.x)||(P.x == R2.x))))
     return BORDER; 
   return OUTSIDE;
 }
