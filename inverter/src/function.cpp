3a50c9ff8540c8b6b70f4feda96cddc77f5a3420
AngeloGustavo <angelogustavo157@gmail.com>
2021-06-15 23:24:50 -0300

    Q9

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,5 +1,5 @@
-#include "function.h"
-
+//#include "function.h"
+#include <array>
 #include <iterator>
 using std::iter_swap;
 
@@ -8,7 +8,12 @@ using std::iter_swap;
  * @param arr Reference to the array with the values.
  */
 template <size_t SIZE>
-void reverse( std::array< std::string, SIZE > & arr )
+void reverse( std::array<std::string,SIZE> & arr )
 {
-    /*codigo*/
+    std::string aux="";
+    for(int i=0;i<arr.size()/2;i++){
+      swap(aux,arr[i]);
+      swap(arr[i],arr[arr.size()-i-1]);
+      swap(arr[arr.size()-i-1],aux);
+    } 
 }
